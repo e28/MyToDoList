a11a157ea4ccccea35414d445a29ec43149c7f6e
dannysabag <dannysabag@gmail.com>
2016-09-16 00:56:00 +0300

    Initial Commit

---
+++ MyToDoList/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyToDoList
+//
+//  Created by Danny Sabag on 9/16/16.
+//  Copyright © 2016 Danny Sabag. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
