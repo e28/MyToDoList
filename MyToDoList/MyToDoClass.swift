f2566b0a355beaef5b974702a2aeb94f46aefeb1
Erez Weiss <erezweiss25@gmail.com>
2017-04-17 11:18:50 +0300

    first commit

---
+++ MyToDoList/MyToDoClass.swift
@@ -0,0 +1,13 @@
+//
+//  MyToDoClass.swift
+//  MyToDoList
+//
+//  Created by Danny Sabag on 9/16/16.
+//  Copyright © 2016 Danny Sabag. All rights reserved.
+//
+
+import UIKit
+
+class MyToDoClass: NSObject {
+
+}
